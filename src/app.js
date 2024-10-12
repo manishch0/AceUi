06057c82785871ad4d7850aefc9e6dc884325548
Manish chaudhary <manishchaudhary@Manish-MacBook.local>
2024-10-12 22:52:52 +0530

    checkbox test file

--- src/app.js
+++ src/app.js
@@ -8,13 +8,6 @@ function App() {
             <Checkbox label={"apple"} check={check} onChange={(value) => {
                 setCheck(value)
             }} />
-            <Checkbox label={"banana"} check={check} onChange={(value) => {
-                setCheck(value)
-            }} /><Checkbox label={"orange"} check={check} onChange={(value) => {
-                setCheck(value)
-            }} /><Checkbox label={"test the label"} check={check} onChange={(value) => {
-                setCheck(value)
-            }} />
         </div>
     )
 }
